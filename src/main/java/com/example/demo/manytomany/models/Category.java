7b198d3e5a26e825332c8660e341e908f1835ba5
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-23 20:22:42 -0300

    category

--- src/main/java/com/example/demo/manytomany/models/Category.java
+++ src/main/java/com/example/demo/manytomany/models/Category.java
@@ -1,5 +1,60 @@
 package com.example.demo.manytomany.models;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.example.demo.Constants;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+@Entity
 public class Category {
 
+	@Id
+    @GeneratedValue(generator = Constants.ID_GENERATOR)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    @JoinTable(
+            name = "CATEGORY_ITEM",
+            joinColumns = @JoinColumn(name = "CATEGORY_ID"),
+            inverseJoinColumns = @JoinColumn(name = "ITEM_ID")
+    )
+    private Set<Item> items = new HashSet<>();
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Item> getItems() {
+        return Collections.unmodifiableSet(items);
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
 }
