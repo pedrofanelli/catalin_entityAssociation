e25b0d856cf8cfa6ab3af4d0407e35470077adb5
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-23 19:03:04 -0300

    repository

--- src/main/java/com/example/demo/onetomany/repositories/BidRepository.java
+++ src/main/java/com/example/demo/onetomany/repositories/BidRepository.java
@@ -1,5 +1,12 @@
 package com.example.demo.onetomany.repositories;
 
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.onetomany.models.Bid;
+import com.example.demo.onetomany.models.Item;
+
 public interface BidRepository extends JpaRepository<Bid, Long> {
     Set<Bid> findByItem(Item item);
 }
